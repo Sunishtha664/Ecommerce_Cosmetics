b890a32e2aa9df430f5ab18ba0024b92eea817b6
Sunishtha664 <sunishthatuladhar22@gmail.com>
2026-04-02 12:02:32 +0545

    Initial commit - MERN project

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+
+//config env
+dotenv.config();
+
+//rest object
+const app = express();
+
+//rest api
+app.get('/', (req, res) => {
+    res.send(
+        "<h1>Welcome to Ecommerce App</h1>"
+    );
+});
+
+//port
+const PORT = process.env.PORT || 3000;
+
+//run listen
+app.listen(PORT, () => {
+    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
+});
