4380c51abecf8079c549fb6d60a41329b9ef4149
Sunishtha664 <sunishthatuladhar22@gmail.com>
2026-04-02 14:21:57 +0545

    working

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import colors from 'colors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
+import authRoute from './routes/authRoute.js';
 
 //config env
 dotenv.config();
@@ -17,6 +18,9 @@ const app = express();
 //middelware
 app.use(express.json());
 app.use(morgan('dev'));
+
+//routes
+app.use('/api/v1/auth', authRoute);
 
 //rest api
 app.get('/', (req, res) => {
