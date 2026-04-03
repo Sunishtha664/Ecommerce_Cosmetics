374b5b5172bec255c258ae90b66add0d8841fc0d
Sunishtha664 <sunishthatuladhar22@gmail.com>
2026-04-03 14:39:05 +0545

    working

--- controller/authController.js
+++ controller/authController.js
@@ -106,3 +106,7 @@ export const loginController = async (req, res) => {
     }
 }
 
+//test controller
+export const testController = (req, res) => {
+    res.send("Protected Routes");
+}
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { registerController, loginController } from '../controller/authController.js';
-
+import { registerController, loginController, testController } from '../controller/authController.js';
+import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js';
 //router object
 const router = express.Router();
 
@@ -12,5 +12,5 @@ router.post('/register', registerController);
 router.post('/login', loginController);
 
 //test route
-router.get('/test', testController);
+router.get('/test', requireSignIn, isAdmin, testController);
 export default router;
