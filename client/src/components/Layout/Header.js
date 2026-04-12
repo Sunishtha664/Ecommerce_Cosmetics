c5cdab23dc5369a00a1571227b3c179d44c55340
Sunishtha664 <sunishthatuladhar22@gmail.com>
2026-04-12 21:38:17 +0545

    working

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,10 +1,44 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header comp</h1>
-        </div>
+        <>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                        <NavLink className="navbar-brand" to="/" href="#">
+                            Hidden brand
+                        </NavLink>
+
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink className="nav-link active" aria-current="page" to="/" href="#">
+                                    Home
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/about" href="#">
+                                    About
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/contact" href="#">
+                                    Contact
+                                </NavLink>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+            </nav>
+
+        </>
     )
 }
 
