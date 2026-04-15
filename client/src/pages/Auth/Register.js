c52f690305bf05e55ec7cb31ae4a7872d7b8b3cd
Sunishtha664 <sunishthatuladhar22@gmail.com>
2026-04-15 15:37:48 +0545

    working

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,25 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../../components/Layout/Layout'
 
 const Register = () => {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [phone, setPhone] = useState("")
+    const [address, setAddress] = useState("")
     return (
         <Layout title="Register Page">
             <div className="register">
                 <h1>Registration Form</h1>
                 <form>
                     <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="form-control" id="exampleInputName" placeholder='Enter Your Name' required />
 
                     </div>
                     <div className="mb-3">
-                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" id="exampleInputName" placeholder='Enter Your Email' required />
+
                     </div>
-                    <div className="mb-3 form-check">
-                        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+
+                    <div className="mb-3">
+                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" id="exampleInputPassword1" placeholder='Enter Your Password' required />
                     </div>
+
+                    <div className="mb-3">
+                        <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} className="form-control" id="exampleInputName" placeholder='Enter Your Phone Number' required />
+
+                    </div>
+                    <div className="mb-3">
+                        <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} className="form-control" id="exampleInputName" placeholder='Enter Your Address' required />
+
+                    </div>
+
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
 
