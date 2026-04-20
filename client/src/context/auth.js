2fed5d361e41eda690542e73c8949ad7dee8b79f
Sunishtha664 <sunishthatuladhar22@gmail.com>
2026-04-20 09:23:28 +0545

    working

---
+++ client/src/context/auth.js
@@ -0,0 +1,10 @@
+import { useState, useEffect, useContext, createContext } from 'react';
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+    const [auth, setAuth] = useState({
+        user: null,
+        token: ""
+    });
+}
