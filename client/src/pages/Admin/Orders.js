cdeab815361dc423df83b20173b35c2c121797f7
Sunishtha664 <sunishthatuladhar22@gmail.com>
2026-04-28 11:35:10 +0545

    working

--- client/src/pages/Admin/Orders.js
+++ client/src/pages/Admin/Orders.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import Layout from '../../components/Layout/Layout'
+import AdminMenu from '../../components/Layout/AdminMenu'
 
 const Orders = () => {
     return (
-        <div>
-            <h1>Orders Management</h1>
-        </div>
+        <Layout title="Dashboard-All Orders">
+            <div className="container-fluid" m-3 p-3>
+                <div className="row">
+                    <div className="col-md-3">
+                        <AdminMenu />
+                    </div>
+                    <div className="col-md-9">
+                        <h1>All Orders</h1>
+                    </div>
+
+                </div>
+            </div>
+        </Layout>
     )
 }
 
