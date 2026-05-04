ee8cfc5fa8bb03fa3d09241fa7b12dd49c709a0d
Sunishtha664 <sunishthatuladhar22@gmail.com>
2026-05-04 11:07:07 +0545

    working

--- controller/productController.js
+++ controller/productController.js
@@ -1,9 +1,38 @@
 import productModel from "../models/productModel.js";
+import fs from 'fs';
+import slugify from "slugify";
 
 
 export const createProductController = async (req, res) => {
     try {
-        const products = await new productModel({ ...req.body }).save();
+        const { name, slug, description, price, category, quantity, shipping } = req.fields;
+        const { photo } = req.files;
+
+        //validation
+        switch (true) {
+            case !name:
+                return res.status(500).send({ error: "Name is required" });
+            case !description:
+                return res.status(500).send({ error: "Description is required" });
+            case !price:
+                return res.status(500).send({ error: "Price is required" });
+            case !category:
+                return res.status(500).send({ error: "Category is required" });
+            case !quantity:
+                return res.status(500).send({ error: "Quantity is required" });
+            case !photo:
+                return res.status(500).send({ error: "Photo is required" });
+            case photo && photo.size > 1000000:
+                return res.status(500).send({ error: "Photo is required and should be less than 1mb in size" });
+        }
+
+        const products = new productModel({ ...req.fields, slug: slugify(name) })
+        if (photo) {
+            products.photo.data = fs.readFileSync(photo.path);
+            products.photo.contentType = photo.type;
+        }
+        await products.save();
+
         res.status(201).send({
             success: true,
             message: "New product created",
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { createProductController } from '../controller/productController.js';
 import { requireSignIn, isAdmin } from '../middlewares/authMiddleware.js';
+import formidable from 'express-formidable';
 
 const router = express.Router();
 
 //routes
-router.post('/create-product', requireSignIn, isAdmin, createProductController);
+router.post('/create-product', requireSignIn, isAdmin, formidable(), createProductController);
 
 export default router;
