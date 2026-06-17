3443e41ab501bccc2e62b8ff501757ebe7f3fb8b
Sunishtha664 <sunishthatuladhar22@gmail.com>
2026-06-17 10:22:50 +0545

    working

--- client/src/pages/Categories.js
+++ client/src/pages/Categories.js
@@ -4,16 +4,16 @@ import useCategory from '../hooks/useCategory'
 import Layout from '../components/Layout/Layout'
 
 const Categories = () => {
-    const categories = useCategory()
+    const { categories } = useCategory()
     return (
         <Layout title={"All Categories - Best offers on Cosmetics"}>
             <div className="container">
                 <div className="row">
-                    {categories.map(c => (
-                        <div className="col-md-6" key={c._id}>
-                            <button className="btn btn-outline-primary">
-                                <Link to="/">{c.name}</Link>
-                            </button>
+                    {categories.map((c) => (
+                        <div className="col-md-6 mt-5 mb-3 gx-3 gy-3" key={c._id}>
+                            <Link to={`/category/${c.slug}`} className="btn btn-outline-primary">
+                                {c.name}
+                            </Link>
                         </div>
                     ))}
                 </div>
