b8c117cf11889c4575773a732b4c49a397c09fa0
Sunishtha664 <sunishthatuladhar22@gmail.com>
2026-06-18 14:40:28 +0545

    working

--- client/src/App.js
+++ client/src/App.js
@@ -35,6 +35,7 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='/product/:slug' element={<ProductDetails />} />
         <Route path='/categories' element={<Categories />} />
+        <Route path='/cart' element={<CartPage />} />
         <Route path='/category/:slug' element={<CategoryProduct />} />
         <Route path='/search' element={<Search />} />
 
---
+++ client/src/pages/CartPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const CartPage = () => {
+    return (
+        <Layout>
+            <div className="container mt-3">
+
+                <div className="row">
+                    <h1 className="text-center">Cart Page</h1>
+                </div>
+            </div>
+
+        </Layout>
+    )
+}
+
+export default CartPage
