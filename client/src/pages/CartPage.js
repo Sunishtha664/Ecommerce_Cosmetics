f49402de3064b68acc7e5a4e94e0062e58681296
Sunishtha664 <sunishthatuladhar22@gmail.com>
2026-07-08 21:42:11 +0545

    working

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -78,11 +78,30 @@ const CartPage = () => {
                         <hr />
 
                         <h4>Total: रु{totalPrice()}</h4>
+                        {auth?.user?.address ? (
+                            <>
+                                <div className='mb-3'>
+                                    <h4>Current Address</h4>
+                                    <h5>{auth?.user?.address}</h5>
+                                    <button className='btn btn-outline-warning' onClick={() => navigate('/dashboard/user/profile')}>Update Address</button>
+                                </div>
+
+                            </>
+                        ) : (
+                            <div className='mb-3'>
+                                {auth?.token ? (
+                                    <button className='btn btn-outline-warning' onClick={() => navigate('/dashboard/user/profile')}>Update Address</button>
+                                ) : (
+                                    <button className='btn btn-outline-warning' onClick={() => navigate('/login', { state: "/cart" })}>Please Login to Checkout</button>
+                                )}
+                            </div>
+                        )}
                     </div>
+
                 </div>
             </div>
 
-        </Layout>
+        </Layout >
     )
 }
 
